d47e681c5bedd405cde8ad570f3ab3a191b2a121
keypass <saibhargav.304@gmail.com>
2025-03-14 08:27:07 +0530

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,11 @@
+document.write(`
+<footer>
+    <p>&copy; 2025 ABK Global Technologies. All Rights Reserved.</p>
+    <div class="social-icons">
+        <a href="https://www.facebook.com" target="_blank"><i class="fab fa-facebook-f"></i></a>
+        <a href="https://www.instagram.com" target="_blank"><i class="fab fa-instagram"></i></a>
+        <a href="https://www.youtube.com" target="_blank"><i class="fab fa-youtube"></i></a>
+        <a href="https://www.twitter.com" target="_blank"><i class="fab fa-twitter"></i></a>
+    </div>
+</footer>
+`);
