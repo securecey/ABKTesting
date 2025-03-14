a3dcc9ccc4152c2f17ad2119f81c1e26ca6879a3
keypass <saibhargav.304@gmail.com>
2025-03-14 08:57:34 +0530

    Update header.js

--- header.js
+++ header.js
@@ -15,11 +15,30 @@ document.write(`
                             <li><a href="#">Gateway Level Security - Palo Alto, Fortinet, SonicWALL, Checkpoint, Sophos, Cisco</a></li>
                         </ul>
                     </li>
+
                     <li class="has-sub-dropdown">
-                        <a href="#">Data Management Solutions</a>
+                        <a href="#">IT Networking Solutions</a>
                         <ul class="sub-dropdown">
-                            <li><a href="#">NAS – Dell EMC, HP, Netgear, QNAP.</a></li>
-                            <li><a href="#">SAN – Dell EMC, IBM, Pure Storage, NetApp, HP.</a></li>
+                            <li><a href="#">Switches – Cisco, Arista, Ruckus, Dell EMC, Brocade, Netgear</a></li>
+                            <li><a href="#">Wireless Solutions – Arista, Aruba, Cisco, Ruckus</a></li>
+                            <li><a href="#">Structured Cabling – CommScope, Panduit, Belden, Molex, etc</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has-sub-dropdown">
+                        <a href="#">Backup & Archival Solutions</a>
+                        <ul class="sub-dropdown">
+                            <li><a href="#">Backup – DellEMC Data Protection, VERITAS NetBackup, Commvault</a></li>
+                            <li><a href="#">Backup Appliance – VERITAS NetBackup, Dell EMC Power Protect</a></li>
+                        </ul>
+                    </li>
+
+                    <li class="has-sub-dropdown">
+                        <a href="#">Virtualization Solutions</a>
+                        <ul class="sub-dropdown">
+                            <li><a href="#">Server Virtualization – VMware, Hyper-V, Citrix, RHEL Desktop</a></li>
+                            <li><a href="#">Virtualization (VDI) – VMware, Hyper-V, Citrix</a></li>
+                            <li><a href="#">Virtualization (VSAN) – VMware</a></li>
                         </ul>
                     </li>
                 </ul>
