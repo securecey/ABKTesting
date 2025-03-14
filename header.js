781c45c4364b1a7505aceefd606b2e1c14f0c8f9
keypass <saibhargav.304@gmail.com>
2025-03-14 08:22:48 +0530

    Update and rename script.js to header.js

---
+++ header.js
@@ -0,0 +1,14 @@
+document.write(`
+<header>
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="#">Solutions</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Partners</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="contact.html">Contact Us</a></li>
+        </ul>
+    </nav>
+</header>
+`);
