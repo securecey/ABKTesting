10faf5b2f95fa89732099b476c91ef6c0bd37f1b
keypass <saibhargav.304@gmail.com>
2025-03-14 08:41:17 +0530

    Update header.js

--- header.js
+++ header.js
@@ -1,12 +1,52 @@
 document.write(`
-<header>
+<header id="main-header">
     <nav>
         <ul>
             <li><a href="index.html">Home</a></li>
-            <li><a href="#">Solutions</a></li>
-            <li><a href="#">Services</a></li>
+
+            <li class="has-dropdown">
+                <a href="#">Solutions</a>
+                <ul class="dropdown">
+                    <li class="has-sub-dropdown">
+                        <a href="it_security.html">IT Security Solutions</a>
+                        <ul class="sub-dropdown">
+                            <li><a href="#">Endpoint Security – TrendMicro, Kaspersky, McAfee, Sophos.</a></li>
+                            <li><a href="#">Encryption - TrendMicro, WinMagic, McAfee</a></li>
+                            <li><a href="#">Gateway Level Security - Palo Alto, Fortinet, SonicWALL, Checkpoint, Sophos, Cisco</a></li>
+                        </ul>
+                    </li>
+                    <li class="has-sub-dropdown">
+                        <a href="#">Data Management Solutions</a>
+                        <ul class="sub-dropdown">
+                            <li><a href="#">NAS – Dell EMC, HP, Netgear, QNAP.</a></li>
+                            <li><a href="#">SAN – Dell EMC, IBM, Pure Storage, NetApp, HP.</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="has-dropdown">
+                <a href="#">Services</a>
+                <ul class="dropdown">
+                    <li><a href="#">Consulting</a></li>
+                    <li><a href="#">Managed Services</a></li>
+                    <li><a href="#">Cloud Solutions</a></li>
+                    <li><a href="#">Support & Maintenance</a></li>
+                </ul>
+            </li>
+
             <li><a href="#">Partners</a></li>
-            <li><a href="#">About Us</a></li>
+
+            <li class="has-dropdown">
+                <a href="#">About Us</a>
+                <ul class="dropdown">
+                    <li><a href="#">Our Mission</a></li>
+                    <li><a href="#">Our Clientele</a></li>
+                    <li><a href="#">Our Core Competence</a></li>
+                    <li><a href="#">Our Goal</a></li>
+                </ul>
+            </li>
+
             <li><a href="contact.html">Contact Us</a></li>
         </ul>
     </nav>
